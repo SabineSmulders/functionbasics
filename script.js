20e0f1962ff9aa872e449e73f2c983e76617f864
SabineSmulders <92474494+SabineSmulders@users.noreply.github.com>
2022-01-07 11:57:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// function = list of steps, optional name, you can 'call' a function 
+
+// Defining a function
+const makePizza = function () {
+    console.log('Prepare a pizza base');
+    console.log('Layer it with tomato sauce');
+    console.log('Put your favorite toppings on it'); 
+
+}
+
+// Using or calling a function 
+makePizza();
+makePizza();
+
+const makeSushi = function () {
+    console.log('Boil the sushi rice');
+    console.log('Use you favorite fish');
+    console.log('Roll it, baby!')
+}
+makeSushi(); 
+makeSushi();
+makeSushi();
